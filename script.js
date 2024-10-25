d4e18dc18152dcde707f17f731c7561290f2ba1f
Kayla Wilson <kaylazwilsonbusiness@gmail.com>
2024-10-25 10:02:07 -0400

    Done

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 console.log("Script Running");
 
-
-
 const winnerDiv = document.querySelector("#winner");
+const restartButton = document.querySelector("#restart-button"); // Assuming you have a restart button
 
-
-let blue_position = 1;
-let pink_position = 1;
-let brown_position = 1;
-
-// Query Selectors Here
-const horses = {
-  blue:{
+let horses = {
+  blue: {
     position: 1,
-    button: document.querySelector("#bllue-button"),
+    button: document.querySelector("#blue-button"),
     horse: document.querySelector("#blue-horse")
   },
   pink: {
@@ -26,27 +19,9 @@ const horses = {
     button: document.querySelector("#brown-button"),
     horse: document.querySelector("#brown-horse")
   }
-
 };
 
-// OnClick Functions Here
-const advanceBlue = (e) => {
-  blue_position += 1;
-  changePosition(blueHorse, blue_position);
-  checkWinner(blue_position, "Blue"); // Check for winner
-};
-
-const advancePink = (e) => {
-  pink_position += 1;
-  changePosition(pinkHorse, pink_position);
-  checkWinner(pink_position, "Pink"); // Check for winner
-};
-
-const advanceBrown = (e) => {
-  brown_position += 1;
-  changePosition(brownHorse, brown_position);
-  checkWinner(brown_position, "Brown"); // Check for winner
-};
+console.log(horses);
 
 // Check for a winner
 const checkWinner = (position, color) => {
@@ -57,25 +32,36 @@ const checkWinner = (position, color) => {
     Object.values(horses).forEach(horse => horse.button.disabled = true);
     restartButton.style.display = "block";
   }
-  
 };
 
-const restartGame =() => {
-  Object.values(horses).forEach(horse=> {
-    horse.position = 1;
-    horse.button.disabled = false;
-    changePosition(horse.horse ,horse.position);
-  });
-  winnerDiv.innerHTML="";
-  restartGame.style.display= "none";
-}
 const changePosition = (horse, position) => {
   if (position <= 5) {
     horse.style.setProperty('grid-column', position);
   }
 };
-// Event Listeners Here
-blueButton.addEventListener("click", advanceBlue);
-pinkButton.addEventListener("click", advancePink);
-brownButton.addEventListener("click", advanceBrown);
-restartButton.addEventListener("click",restartGame);
+
+const advance = (e) => {
+  const color = e.target.dataset.color; // Use data attribute for color
+  horses[color].position += 1;
+  changePosition(horses[color].horse, horses[color].position);
+  horses[color].horse.alt = `${color} horse at position ${horses[color].position} out of 5`;
+  checkWinner(horses[color].position, color);
+};
+
+const restartGame = () => {
+  Object.values(horses).forEach(horse => {
+    horse.position = 1;
+    horse.button.disabled = false;
+    changePosition(horse.horse, horse.position);
+  });
+  winnerDiv.innerHTML = "";
+  restartButton.style.display = "none";
+};
+
+// Event Listeners
+Object.values(horses).forEach(horse => {
+  horse.button.addEventListener("click", advance);
+});
+
+// Restart button event listener
+restartButton.addEventListener("click", restartGame);
